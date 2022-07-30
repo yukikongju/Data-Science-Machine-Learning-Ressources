94be3e980aa31a7b69787d7ec84a6b04058a7e52
Emulie Chhor <emulie.chhor@hotmail.com>
2022-07-30 11:17:56 -0400

    added batch norm and dropout net

--- books/DLWithPython-Stevens/chap8/README.md
+++ books/DLWithPython-Stevens/chap8/README.md
@@ -6,7 +6,9 @@
     - [X] With AdaptiveMaxPool2d
     - [ ] With Dropout2D
     - [ ] With BatchNorm2D
-    - [ ] With Regularization and weight penalties/decay
+- [ ] Using regularization/weight decay
+    - [ ] computing weight decay inside training loop
+    - [ ] initialize weight_decay inside optimizer
 - [ ] Comparing our models
 - [ ] Saving our model
 
@@ -18,6 +20,7 @@
 - Conv2D is of size [out_ch x in_ch x 3 x 3] (ie Batch x Channels x Height x Width)
     * image size : [1, 3, 32, 32]
 - To figure out how many neurons our Linear layer has after the Conv2D layer:
-    * ` nn.Linear( last_out_conv2d x 8 * 8 )`, where 8 is 32/(2**num_conv2d_layers)
+    * ` nn.Linear( last_out_conv2d x 8 * 8 )`, where 8 is `32/(2**num_conv2d_layers)`
+    * `nn.Linear (last_out x last_out x num_channels // 2)`
     * We can use `nn.AdaptiveMaxPool2d((5,7))` to fix image to 5x7 
 
--- books/DLWithPython-Stevens/chap8/model.py
+++ books/DLWithPython-Stevens/chap8/model.py
@@ -82,6 +82,50 @@ class CNN(nn.Module):
         return x
 
 
+class DropoutNet(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super(DropoutNet, self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=3)
+        self.dropout = nn.Dropout2d(p=0.3)
+        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=2)
+        self.fct1 = nn.Linear(32 * 4 * 4, 12)
+        self.fct2 = nn.Linear(12, out_channels)
+        
+
+    def forward(self, x):
+        x = self.pool(torch.tanh(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.dropout(x)
+        x = x.reshape(x.shape[0], -1)
+        x = torch.tanh(self.fct1(x))
+        x = self.fct2(x)
+        return x
+
+
+class BatchNormNet(nn.Module):
+
+    def __init__(self, in_channels, out_channels, n_channels):
+        super(BatchNormNet, self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=3)
+        self.norm1 = nn.BatchNorm2d(num_features=n_channels)
+        self.norm2 = nn.BatchNorm2d(num_features=n_channels // 2)
+        self.conv1 = nn.Conv2d(in_channels, n_channels, kernel_size=3, padding=2)
+        self.conv2 = nn.Conv2d(n_channels, n_channels//2, kernel_size=3, padding=2)
+        self.fct1 = nn.Linear(n_channels // 2 * 4 * 4, out_channels)
+
+    def forward(self, x):
+        x = self.norm1(self.conv1(x))
+        x = self.pool(x)
+        x = self.norm2(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fct1(x)
+        return x
+
+
 def test_cnn():
     # test CNN with colored img 32x32
     t = torch.randn(3, 32, 32).unsqueeze(0) 
@@ -113,10 +157,23 @@ def test_depthnet():
     model = DepthNet(in_channels=3, out_channels=10)
     print(model(t))
     
+def test_dropoutnet():
+    t = torch.randn(3, 32, 32).unsqueeze(0) 
+    model = DropoutNet(in_channels=3, out_channels=10)
+    print(model(t))
+    
+def test_batchnormnet():
+    t = torch.randn(3, 32, 32).unsqueeze(0) 
+    model = BatchNormNet(in_channels=3, out_channels=10, n_channels=32)
+    print(model(t))
+    
+
 if __name__ == "__main__":
     test_cnn()
     test_convnet()
     test_adaptnet()
     test_depthnet()
+    test_dropoutnet()
+    test_batchnormnet()
 
 
