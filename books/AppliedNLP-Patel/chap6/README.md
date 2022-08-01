bdfad84391ba4fd141b970b7ddac049f1363d4ac
Emulie Chhor <emulie.chhor@hotmail.com>
2022-08-01 12:43:30 -0400

    added bidirectional rnn

--- books/AppliedNLP-Patel/chap6/README.md
+++ books/AppliedNLP-Patel/chap6/README.md
@@ -2,12 +2,13 @@
 
 [Code](https://github.com/nlpbook/nlpbook/blob/main/ch06.ipynb)
 
+## Contents
 
 - [o] Sequence Modeling using
     - [X] Dummy RNN (no embedding)
     - [X] Reccurent Neural Neworks (RNNs)
     - [X] RNN Embedding flatten (?)
-    - [ ] Bidirectionals RNNs
+    - [X] Bidirectionals RNNs
     - [ ] Long-Short Term Memory (LSTMs)
     - [ ] Gated Recurrent Units (GRUs)
 - [ ] More Models
@@ -15,12 +16,21 @@
     - [ ] QRNNs
     - [ ] SHA-RNNs
 
+
 **To Research**
 - [ ] Why is there a difference between RNNEmbeding and RNNEmbeddingFlatten?
+- [ ] Should we use `reshape()` or `view()` to reshape? what dimension 
+      should middle layer have?
 
 
 **Notes**
 
+- The output size of the last `nn.Linear()` layer should depend on the task 
+  we want to accomplish:
+    - predict next word: the last layer need to output the probability for 
+      each word in the vocab, so we need `nn.Linear( .. , vocab_size )`
+    - predict context: the last layer need to output the indices for the 
+      dictionary, so we need `nn.Linear( .. , context_dim )`
 
 
 ## Ressources
--- books/AppliedNLP-Patel/chap6/model.py
+++ books/AppliedNLP-Patel/chap6/model.py
@@ -29,7 +29,7 @@ class RNNEmbedding(nn.Module):
         self.fct2 = nn.Linear(8, vocab_size)
 
     def forward(self, x, h=None):
-        x = self.embed(x).unsqueeze(0) # size: [1 x 1 x contex_dim]
+        x = self.embed(x).unsqueeze(0) # size: [1 x 1 x context_dim]
         if h is None:
             _, x = self.rnn(x)
         else: 
@@ -49,16 +49,43 @@ class RNNEmbeddingFlatten(nn.Module):
         self.fct2 = nn.Linear(8, vocab_size)
 
     def forward(self, x, h=None):
-        x = self.embed(x).unsqueeze(0) # size: [1 x 1 x contex_dim]
+        x = self.embed(x).unsqueeze(0) # size: [1 x 1 x context_dim]
         if h is None:
             _, x = self.rnn(x)
         else: 
             _, x = self.rnn(x, h)
-        x = x.reshape(x.shape[0], -1)
+        #  x = x.reshape(x.shape[0], -1) # size: [1 x (embedding_dim * context_dim)]
+        x = x.view((1,-1))
         x = self.fct1(x)
         x = torch.sigmoid(self.fct2(x))
         return x
 
+
+class BidirectionalRNN(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim, context_dim):
+        super(BidirectionalRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.RNN(embedding_dim, embedding_dim, batch_first=True, 
+                bidirectional=True)
+        self.fct1 = nn.Linear(embedding_dim * context_dim, 12)
+        self.fct2 = nn.Linear(12, vocab_size)
+
+    def forward(self, x):
+        x = self.embed(x).unsqueeze(0)
+        _, x = self.rnn(x)
+        # size: [1 x (embedding_dim*context_dim)] ; [(embedding_dim*context_dim) x 12]
+        x = torch.cat((x[0], x[1]), dim=-1) 
+        x = self.fct1(x)
+        x = torch.tanh(self.fct2(x))
+        return x
+
+def test_bidirectional_rnn():
+    vocab_size, embedding_dim, context_dim = 50, 10, 2
+    t = torch.tensor([4, 18]) # size: 1 x context_dim
+    model = BidirectionalRNN(vocab_size, embedding_dim, context_dim)
+    print(model(t))
+        
 
 def test_nn_embedding_flatten():
     vocab_size, embedding_dim, context_dim = 50, 10, 2
@@ -103,6 +130,7 @@ if __name__ == "__main__":
     #  test_rnn()
     #  test_dummy_rnn()
     #  test_dummy_rnn_embedding()
-    test_rnn_embedding()
+    #  test_rnn_embedding()
     test_nn_embedding_flatten()
+    test_bidirectional_rnn()
 
