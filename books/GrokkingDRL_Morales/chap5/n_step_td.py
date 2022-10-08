311722600ceba3f9df4580d1fc8e177c91973f24
Emulie Chhor <emulie.chhor@hotmail.com>
2022-10-07 20:52:44 -0400

    added backward td lambda agent's evaluation

--- books/GrokkingDRL_Morales/chap5/n_step_td.py
+++ books/GrokkingDRL_Morales/chap5/n_step_td.py
@@ -43,10 +43,54 @@ def n_step_td(pi, env, gamma=1.0, init_alpha=0.5, min_alpha=0.01,
     return V, V_track
     
 
+def forward_td(pi, env, lambda_= 0.95, gamma=1.0):
+    """ 
+    We are weighting each experience differently
+    """
+    pass
+    
+
+def backward_td(pi, env, gamma=1.0, init_alpha=0.05, min_alpha=0.01, 
+        alpha_decay_ratio=0.5, lambda_=0.3, n_episodes=500):
+    """ Decay Elligibility Traces """
+    # initialize variables
+    nS = env.observation_space.n
+    V = np.zeros(shape=(nS,), dtype=np.float64)
+    V_track = np.zeros(shape=(n_episodes, nS), dtype=np.float64)
+    E = np.zeros(shape=(nS,), dtype=np.float64) # elligibility traces
+
+    alphas = decay_schedule(init_alpha, min_alpha, alpha_decay_ratio, n_episodes)
+    #  discounts = np.logspace(0, n_steps +1, base=gamma, endpoint=False)
+
+
+    for e in tqdm(range(n_episodes)):
+        E.fill(0)
+        state, done = env.reset(), False
+
+        # generate trajectory
+        while not done:
+            action = get_action(pi, state)
+            next_state, reward, done, _ = env.step(action)
+            E[state] += 1
+
+            td_target = reward + gamma * V[next_state] * (not done)
+            td_error = td_target - V[state]
+            V[state] = V[state] + alphas[e] * td_error * E[state]
+            E[state] = E[state] * gamma * lambda_
+
+            state = next_state
+        
+        V_track[e] = V
+
+    return V, V_track
+    
+
+
 def main():
     env = gym.make('FrozenLake-v1')
     pi = get_random_stochastic_policy(env)
-    V, V_track = n_step_td(pi, env, gamma=0.99, n_steps=10)
+    #  V, V_track = n_step_td(pi, env, gamma=0.99, n_steps=10)
+    V, V_track = backward_td(pi, env, gamma=0.95)
     print(V)
 
 
