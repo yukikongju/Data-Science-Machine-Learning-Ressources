9bf01acbf2a8a59e752cd171b0f3736b3f73cfd5
Emulie Chhor <emulie.chhor@hotmail.com>
2022-10-23 16:13:00 -0400

    added sarsa

--- books/GrokkingDRL_Morales/chap6/improving_agents.py
+++ books/GrokkingDRL_Morales/chap6/improving_agents.py
@@ -78,12 +78,50 @@ def mc_control(env, gamma=1.0, init_alpha=0.5, min_alpha=0.01,
     return Q, V, pi, Q_track, pi_track
 
 
+def select_action(state, Q, epsilon):
+    if np.random.random() > epsilon:
+        action = np.argmax(Q[state])
+    else:
+        action = np.random.randint(len(Q[state]))
+    return action
+
+
+def sarsa(env, gamma=1.0, alpha_init = 0.5, alpha_min = 0.01, 
+          alpha_decay_ratio=0.05, epsilon_init=0.3, epsilon_min=0.01, 
+          epsilon_decay_ratio=0.05, n_episodes=1000, max_steps=200):
+    # initialization
+    nS, nA = env.observation_space.n, env.action_space.n
+    Q = np.zeros((nS, nA), dtype=np.float64)
+    Q_track = np.zeros((n_episodes, nS, nA), dtype=np.float64)
+    alphas = decay_schedule(init_alpha, alpha_min, alpha_decay_ratio, n_steps)
+    epsilons = decay_schedule(init_epsilon, epsilon_min, epsilon_decay_ratio, n_episodes)
+
+    for e in tqdm(range(n_episodes)):
+        state, done = env.reset(), False
+        action = select_action(state, Q, epsilons[e])
+        while not done: 
+            next_state, reward, done, _ = env.step(action)
+            next_action = select_action(next_state, Q, epsilons[e])
+            td_target = reward + gamma * Q[next_state][next_action]
+            td_error = td_target - Q[state][action]
+            Q[state][action] += alphas[e] * td_error
+            state, action = next_state, next_action
+        Q_track[e] = Q
+        pi_track[e] = np.argmax(Q, axis=1)
+
+    V = np.max(Q, axis=1)
+    pi = {s:a for s,a in enumerate(np.argmax(Q, axis=1))}
+
+    return Q, V, pi, Q_track, pi_track
+
+    
 
 
 def main():
     env_name = "FrozenLake-v1"
     env = gym.make(env_name)
     Q, V, pi, Q_track, pi_track = mc_control(env)
+    breakpoint()
     
 
 if __name__ == "__main__":
