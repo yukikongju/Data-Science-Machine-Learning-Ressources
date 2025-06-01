2bed7936fb46a9d74186fb04c91f0c687919cd89
yukikongju <emulie.chhor@hotmail.com>
2025-06-01 00:07:24 -0400

    added inception block naive

--- implementation/models/inception/inception_parts.py
+++ implementation/models/inception/inception_parts.py
@@ -22,13 +22,19 @@ class InceptionBlockNaive(nn.Module):
     As described in the paper "Going Deeper with convolutions a)"
     """
 
-
-    def __init__(self):
+    def __init__(self, in_channels, red_1x1, red_3x3, red_5x5):
         super().__init__()
+        self.branch1 = nn.Conv2d(in_channels, red_1x1, kernel_size=1, stride=1, padding=0)
+        self.branch2 = nn.Conv2d(in_channels, red_3x3, kernel_size=3, stride=1, padding=1)
+        self.branch3 = nn.Conv2d(in_channels, red_5x5, kernel_size=5, stride=1, padding=2)
+        self.branch4 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
 
-
-    def forward(self, x):
-        pass
+    def forward(self, x) -> torch.Tensor:
+        x1 = self.branch1(x)
+        x2 = self.branch2(x)
+        x3 = self.branch3(x)
+        x4 = self.branch4(x)
+        return torch.cat([x1, x2, x3, x4], dim=1)
 
 
 class InceptionBlockV1(nn.Module):
--- implementation/tests/test_inception.py
+++ implementation/tests/test_inception.py
@@ -1,11 +1,11 @@
 import pytest
 import torch
 
-from models.inception.inception_parts import ConvolutionBlock
+from models.inception.inception_parts import ConvolutionBlock, InceptionBlockNaive
 
 @pytest.fixture
 def image() -> torch.Tensor:
-    return torch.randint(0, 255, size=(5, 3, 28, 28)).float()
+    return torch.randint(0, 255, size=(5, 192, 28, 28)).float()
 
 
 def test_convblock(image):
@@ -14,5 +14,12 @@ def test_convblock(image):
     block = ConvolutionBlock(C, OUT_CHANNELS, kernel_size=1)
     output = block(image)
     assert output.size() == (B, OUT_CHANNELS, H, W)
+
+def test_inception_block_naive(image):
+    B, C, H, W = image.size()
+    RED_1x1, RED_3x3, RED_5x5 = 64, 128, 32
+    block = InceptionBlockNaive(in_channels=C, red_1x1=RED_1x1, red_3x3=RED_3x3, red_5x5=RED_5x5)
+    output = block(image)
+    assert output.size() == (B, RED_1x1 + RED_3x3 + RED_5x5 + C, H, W)
 
 
